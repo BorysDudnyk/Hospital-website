18d15a83d92afa88a4f4094021eb6dfe50837485
Borys Dudnyk <100314965+BorysDudnyk@users.noreply.github.com>
2024-05-12 14:59:01 +0300

    Add files via upload

---
+++ Database/registration.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Хост: 127.0.0.1
+-- Время создания: Май 12 2024 г., 14:56
+-- Версия сервера: 5.5.25
+-- Версия PHP: 5.2.12
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- База данных: `hospital_website`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `registration`
+--
+
+CREATE TABLE IF NOT EXISTS `registration` (
+  `name` text NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `doctor` text NOT NULL,
+  `appointmentDay` varchar(99) NOT NULL,
+  `appointmentTime` varchar(99) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
